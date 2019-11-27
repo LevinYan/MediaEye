5ee7bd28e4809f0d902d61cacc6cf498992c1979
levinyang <levinyang@tencent.com>
2019-11-27 10:43:55 +0800

    Initial Commit

---
+++ MediaEye/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  MediaEye
+//
+//  Created by levinyang(杨亦伟) on 2019/11/27.
+//  Copyright © 2019 levinyang(杨亦伟). All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ MediaEye/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  MediaEye
+//
+//  Created by levinyang(杨亦伟) on 2019/11/27.
+//  Copyright © 2019 levinyang(杨亦伟). All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
